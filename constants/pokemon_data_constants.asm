8154ad29bedcfc221b4baa2a16fdd306452b546d
peanutseagull <d.a.seymour04@gmail.com>
2025-07-17 15:18:50 +1000

    Rock Smash Pokemon
    Created data for Rock Smash pokemon

--- constants/pokemon_data_constants.asm
+++ constants/pokemon_data_constants.asm
@@ -223,7 +223,11 @@ DEF NUM_ROAMMON_MAPS EQU 16 ; RoamMaps table size (see data/wild/roammon_maps.as
 	const TREEMON_SET_KANTO
 	const TREEMON_SET_LAKE
 	const TREEMON_SET_FOREST
-	const TREEMON_SET_ROCK
+	const TREEMON_SET_ROCK1
+	const TREEMON_SET_ROCK2
+	const TREEMON_SET_ROCK3
+	const TREEMON_SET_ROCK4
+	const TREEMON_SET_ROCK5
 DEF NUM_TREEMON_SETS EQU const_value
 
 ; treemon scores
